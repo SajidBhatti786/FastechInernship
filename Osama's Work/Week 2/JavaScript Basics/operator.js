8fbb98fba90890bb7da7daf94c4b0797c508a5fe
Osama Bin Azam <osamabinazam@gmail.com>
2023-07-10 16:19:01 +0500

    Operators in Javascript

---
+++ Osama's Work/Week 2/JavaScript Basics/operator.js
@@ -0,0 +1,76 @@
+console.log("\nOperators in Javascript\n")
+
+// Arithmetic Operators
+console.log("Arithmetic Operators\n")
+let a = 10;
+let b = 20;
+console.log("a = "+a)
+console.log("b = "+b)
+console.log("Addition (a + b) = "+(a+b))
+console.log("Subtraction (a - b) = "+(a-b))
+console.log("Multiplication (a * b) = "+(a*b))
+console.log("Division (a / b) = "+(a/b))
+console.log("Modulo (a % b) = "+(a%b))
+console.log("Exponentiation (a ** b) = "+(a**b))
+console.log("Post Increment (a++) = "+(a++))
+console.log("Post Decrement (a--) = "+(a--))
+console.log("Pre-increment (++a) = "+(++a))
+console.log("Pre-decrement (--a) = "+(--a))
+
+
+// Assignment Operator
+console.log("\nAssignment Operator\n")
+let c = 10;
+let d = 20;
+console.log("c = " + c);
+console.log("d = " + d);
+console.log("Addition assignment operator (+=): c += d = " + (c += d));
+console.log("Subtraction assignment operator (-=): c -= d = " + (c -= d));
+console.log("Multiplication assignment operator (*=): c *= d = " + (c *= d));
+console.log("Division assignment operator (/=): c /= d = " + (c /= d));
+console.log("Modulus assignment operator (%=): c %= d = " + (c %= d));
+console.log("Exponentiation assignment operator (**=): c **= d = " + (c **= d));
+
+
+// Comparison Operator
+console.log("\nComparison Operator\n")
+let e = 10;
+let f = 20;
+console.log("e = " + e);
+console.log("f = " + f);
+console.log("Equal to (==): e == f = " + (e == f));
+console.log("Not Equal to (!=): e != f = " + (e != f));
+console.log("Greater than (>): e > f = " + (e > f));
+console.log("Less than (<): e < f = " + (e < f));
+console.log("Greater than or equal to (>=): e >= f = " + (e >= f));
+console.log("Less than or equal to (<=): e <= f = " + (e <= f));
+
+
+//Logical Assignment Operators
+console.log("\nLogical Assignment Operators\n")
+let g = 10;
+let h = 20;
+console.log("g = " + g);
+console.log("h = " + h);
+console.log("Logical AND (&&): g && h = " + (g && h));
+console.log("Logical OR (||): g || h = " + (g || h));
+console.log("Logical NOT (!): !g = " + (!g));
+
+
+// Bitwise Operators
+console.log("\nBitwise Operators\n")
+let i = 10;
+let j = 20;
+console.log("i = " + i);
+console.log("j = " + j);
+console.log("Bitwise AND (&): i & j = " + (i & j));
+console.log("Bitwise OR (|): i | j = " + (i | j));
+console.log("Bitwise XOR (^): i ^ j = " + (i ^ j));
+console.log("Bitwise NOT (~): ~i = " + (~i));
+console.log("Bitwise Left Shift (<<): i << j = " + (i << j));
+console.log("Bitwise Right Shift (>>): i >> j = " + (i >> j));
+console.log("Bitwise Zero fill Right Shift (>>>): i >>> j = " + (i >>> j));
+
+
+
+
